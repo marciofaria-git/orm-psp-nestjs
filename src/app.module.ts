1ccec6c8527c6b99de04ea4976f8811639788502
devmarcio <devmarciofaria@gmail.com>
2023-02-28 14:11:08 -0300

    swagger config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from './config/app.config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true, load: [configuration] })],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Payment Service Provider')
+    .setDescription('cash-in and cash-out management')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
+  const config = app.get<ConfigService>(ConfigService);
+
+  const port = config.get('port');
+
+  await app.listen(port);
 }
 bootstrap();
